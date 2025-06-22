e3b79b7046c889811f23fa4f2d71b671082ac986
Smart Cine Play <smartcineplayoficial@gmail.com>
2025-06-22 19:57:33 -0300

    Delete public directory

--- public/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('search').addEventListener('input', function() {
-  const query = this.value.trim();
-  document.getElementById('result').innerText = query ? 'Buscando por: ' + query : 'Digite algo para buscar.';
-});
